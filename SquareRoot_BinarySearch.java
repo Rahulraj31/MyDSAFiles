b3d0deecbda51410211f1cb2076edf07c18fa0a8
Rahul Raj Pandey <pandeyrahulraj99@gmail.com>
2021-10-11 19:52:47 +0530

    Add files via upload

---
+++ SquareRoot_BinarySearch.java
@@ -0,0 +1,55 @@
+//                  === Finding  Square Root by Binary Search   ====
+
+
+// Time - O(log n)
+
+public class SquareRoot_BinarySearch {
+    public static void main(String[] args) {
+        
+        int n=40;
+        int p =3;      // precision of decimal point
+
+        System.out.printf("%.3f",sqrt(n,p));
+    } 
+
+    static double sqrt(int n, int p){
+
+        int s =0;
+        int e=n;
+
+        double root =0.0;
+
+        while(s<=e){
+            int m= s+(e-s)/2;
+
+            if(m*m ==n){  // perfect root
+                root =m;
+                return root;
+            }
+
+            if(m*m >n){
+                e=m-1;
+            }
+            else{
+                s=m+1;
+            }
+
+            // for precision
+            double incr=0.1;
+            for (int i = 0; i <p; i++) {
+                
+                while(root*root<=n){
+                    root+= incr;
+                }
+
+                root-=incr;
+                incr/=10;   // for 2nd 3rd decimal point
+            }
+            
+
+        }
+
+
+        return root;
+    }
+}
