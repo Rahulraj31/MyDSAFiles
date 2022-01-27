44ea6a4e2e9ddaddbfc89a70c1ea556cd3fd4546
Rahul Raj Pandey <pandeyrahulraj99@gmail.com>
2022-01-27 20:13:54 +0530

    Add files via upload

---
+++ LinkedList/Double_LL/DLL.java
@@ -0,0 +1,212 @@
+//           Custom DOuble Linked List and its operation 
+
+package LinkedList.Double_LL;
+
+public class DLL{
+
+    // node template 
+    private class Node{
+         int value;
+         Node next; 
+         Node prev;   
+    
+        public Node(int val){
+            this.value = val;
+        }
+        
+    }
+    
+    // making head and tail nodes they will also have next and prev
+
+    private Node head;
+    private Node tail;
+    private int size ;
+
+    public DLL(){
+        this.size=0;
+    }
+
+
+    // -----------------------------------------------------------------
+
+    
+
+
+    // == Inserting at First in Linked list == 
+
+    public void insertFirst(int val){
+        // creat new node
+        Node node = new Node(val);
+        node.next = head;
+        node.prev=null;
+
+        if(head!=null){ 
+            head.prev=node;   
+        }
+       
+        head=node;
+        size+=1;
+    }
+
+    // == Inserting at Last in Linked list == 
+
+    public void insertLast(int val){
+        
+        Node node = new Node(val);
+        node.next=null;
+
+        //If the element going to be added will be first
+        if(head==null){ 
+            insertFirst(val);
+            return;
+        }
+
+        Node last =head;
+        while(last.next!=null){
+            last=last.next;
+        }
+
+        last.next=node;
+        node.prev=last; 
+        size+=1;
+    }
+
+
+    // === Insert At any index ===
+
+    public void InsertIndex(int val, int index) {
+        if(index==0){
+            insertFirst(val);
+            return;
+        }
+
+        if(index==size){
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for(int i=1; i<index; i++){
+            temp =temp.next;
+        }
+
+        Node Newnode = new Node(val);
+        Newnode.next=temp.next;
+        temp.next=Newnode; 
+        Newnode.prev=temp;
+        // now attaching the right ele previous to the insertion ele as we done for left ele in above
+        Newnode.next.prev= Newnode;
+        System.out.println();
+        size++;
+    }
+
+
+    // === Get Address of Node in LL ===
+    public Node getAdd(int index){
+        Node ele = head;
+        for(int i=1; i<=index; i++){
+            ele =ele.next;
+        }
+        return ele;
+    }
+
+    // === Get Value of Node in LL ===
+    public int getVal(int index){
+        Node ele = head;
+        for(int i=1; i<=index; i++){
+            ele =ele.next;
+        }
+        return ele.value;
+    }
+
+    // === Find Value  in LL ===
+    public Node find(int val){
+        Node ele = head;
+        while(ele!=null){
+            if(ele.value == val){
+                return ele;
+            }
+            ele=ele.next;
+        }
+        return null;
+    }
+
+
+    // === Delete First ===
+
+    public int deleteFirst() {
+        int val = head.value;
+        head=head.next;
+        if(head==null){  // means if LL has only 1 element
+            tail=null;
+        }
+        size--;
+        return val;
+    }
+
+    // === Delete Last ===
+
+    public void deleteLast() {
+       Node temp= head;
+       for(int i=1; i<size-1; i++){ // iterate to 2nd last element
+           temp=temp.next;
+       }
+       tail=temp;
+       tail.next=null;
+       size--;
+    }
+
+    // === Delete at Index ===
+    public void deleteIndex(int index){
+        if(index == 0){
+            deleteFirst();
+            return;
+        }
+        if(index == size-1){
+            deleteLast();
+            return;
+        }
+
+        Node prev= head;
+        // iterate to the index just before the index to be deleted
+       for(int i=1; i<index; i++){ 
+           prev=prev.next;
+       }
+
+       // now linking prev element to the element after index to be deleted
+       prev.next=prev.next.next; 
+       // means go the element skipping one in between 
+       size--;
+
+    }
+
+
+//-----------------------------------------------------------------
+
+    // Display Size
+    public void size(){
+        System.out.println("The size of Linked List is "+this.size);
+    }
+
+
+    // Display LL
+
+    public void display(){
+        Node temp =  head;
+        Node last=null;
+        while(temp!= null){
+           System.out.print(temp.value + " -> ");
+           last=temp;
+           temp=temp.next;
+        }
+
+        System.out.println("End");
+
+        while(last != null){
+            System.out.print(last.value + " <- ");
+            last=last.prev;
+         }
+         System.out.println("Start");
+    }
+
+}
---
+++ LinkedList/Double_LL/Main.java
@@ -0,0 +1,26 @@
+package LinkedList.Double_LL;
+
+public class Main {
+    public static void main(String[] args) {
+        DLL list = new DLL();
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(4);
+        // list.insertLast(6);
+        // list.InsertIndex(100, 2);
+        // list.display();
+        // System.out.println("Add and Value at index 3 :- "+ list.getAdd(3) +" , "+list.getVal(3));
+        // System.out.println("Value deleted- "+ list.deleteFirst());
+        // list.display();
+        // list.size();
+        // list.deleteLast();
+        // list.display();
+        // list.deleteIndex(1);
+        list.display();
+        list.insertLast(5);
+        list.InsertIndex(6, 2);
+        list.display();
+
+        
+    }
+}
