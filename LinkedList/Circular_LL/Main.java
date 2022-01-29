9b5594fb6d2150a7b0b8c18a20a1382992c7d011
Rahul Raj Pandey <pandeyrahulraj99@gmail.com>
2022-01-29 19:04:29 +0530

    Add files via upload

--- LinkedList/Circular_LL/Main.java
+++ LinkedList/Circular_LL/Main.java
@@ -1,13 +1,13 @@
-package LinkedList.Single_LL;
+package LinkedList.Circular_LL;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(1);
-        list.insertFirst(2);
-        list.insertFirst(4);
-        list.insertLast(6);
-        list.InsertIndex(100, 2);
+        CLL list = new CLL();
+        list.insert(1);
+        list.insert(2);
+        list.insert(4);
+        list.insert(6);
+        // list.InsertIndex(100, 2);
         list.display();
         System.out.println("Add and Value at index 3 :- "+ list.getAdd(3) +" , "+list.getVal(3));
         System.out.println("Value deleted- "+ list.deleteFirst());
