a6c6c8ab0f092a1870786e436e9f939772b729e5
Rahul Raj Pandey <pandeyrahulraj99@gmail.com>
2022-03-15 20:22:07 +0530

    Add files via upload

--- Hashing/Hashing.java
+++ Hashing/Hashing.java
@@ -1,5 +1,5 @@
 
-//                      === Hashing  Hashset ===
+//                      === Hashing ===
 
 /*
 
@@ -11,10 +11,10 @@ HashSet<>()
 HashSet<>(initial capacity)
 HashSet<>(initial capacity, loadFactor)
 */
-
+package Hashing;
 import java.util.*;
 public class Hashing {
-    public static void main(String[] args) {
+    public static void hashset() {
         HashSet<Integer> hs = new HashSet<>();
         hs.add(6);
         hs.add(7);
@@ -40,5 +40,36 @@ public class Hashing {
         hs.clear();
         System.out.println(hs);
 
+    }
+
+    public static void hashmap(){
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        int arr[] = { 10, 34, 5, 10, 3, 5, 10 };
+        for (int i = 0; i < arr.length; i++) {
+            // Get if the element is present
+            Integer c = hmap.get(arr[i]);
+            // If this is first occurrence of element
+            // Insert the element
+            if (hmap.get(arr[i]) == null) {
+                hmap.put(arr[i], 1);
+            }
+            // If elements already exists in hash map
+            // Increment the count of element by 1
+            else {
+                hmap.put(arr[i], ++c);
+            }
+        }
+        for (int i : hmap.keySet()) {
+            System.out.println("key: " + i + " value: " + hmap.get(i));
+        }
+
+        for(Map.Entry m : hmap.entrySet()){    
+            System.out.println(m.getKey()+" "+m.getValue());    
+           }  
+    }
+
+    public static void main(String[] args){
+        hashset();
+        hashmap();
     }
 }
