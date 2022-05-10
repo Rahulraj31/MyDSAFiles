325cb8bf6a6b6171715018c2f735a23eed153fe1
Rahul Raj Pandey <pandeyrahulraj99@gmail.com>
2022-05-10 19:40:55 +0530

    Add files via upload

--- LinkedList/Reversing_LL.java
+++ LinkedList/Reversing_LL.java
@@ -8,28 +8,36 @@ Video Solutions - [1:47:28] https://www.youtube.com/watch?v=70tx7KcMROc&t=6199s
 package LinkedList;
 
 public class Reversing_LL {
-   // Definition for singly-linked list.
-//     private class Node{
-//     private int value;
-//     private Node next;   
-//     public Node(int val){this.value = val;}
-//     public Node(int value, Node next){
-//         this.value = value;
-//         this.next =  next;
-//     }
-// }
-//         int val;
-//         ListNode next;
-//         ListNode() {}
-//         ListNode(int val) { this.val = val; }
-//         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//     }
 
-//     public void recurReverse(ListNode node){
-//         if(node.next!=null ){
-//             head=tail;
-//         }
-//     }
-
+// printing recursively
+     public void recurReverse(Node node){
+         if(node.next!=null ){
+             head=tail;
+            return;
+         }
+         recurReverse(node.next);
+         tail.next=node;
+         tail=node;
+         tail.next=null;
+     }
+	public void  iterReverse(Node head){
+        if(head==null){
+            return head;
+        }      
+        Node curr = head;
+        Node prev = null;
+        Node after = curr.next;
+             
+        while(curr!=null){     // means prev will go till last element     
+            curr.next = prev;  // changing the direction of arrow
+            prev = curr;
+            curr = after;
+            if(after != null){ //  null pointer exception  check for after
+                after = after.next;
+            }
+        }       
+        head = prev;
+        return head;
+    }	
 }
 
